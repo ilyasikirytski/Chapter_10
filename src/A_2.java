2eb92722507a786de548f5ccd6f85704ccfc9968
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-01-20 12:14:02 +0300

    A_2 - task done

---
+++ src/A_2.java
@@ -0,0 +1,23 @@
+/*
+2. Ввести число, занести его цифры в стек. Вывести число, у которого цифры
+идут в обратном порядке.
+ */
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class A_2 {
+    public static void main(String[] args) {
+        Stack<Integer> numbers = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            numbers.push(scanner.nextInt());
+        }
+        for (Integer i : numbers) {
+            String reverse = new StringBuffer(i.toString()).reverse().toString();
+            if (i > Integer.parseInt(reverse)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
