b88d07108d95a84c0a3db2ad3b45d6f4f802491f
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-01-20 22:57:57 +0300

    B_1 - task done

---
+++ src/B_1/B_1_ArrayList.java
@@ -0,0 +1,31 @@
+/*
+В кругу стоят N человек, пронумерованных от 1 до N. При ведении счета
+по кругу вычеркивается каждый второй человек, пока не останется один.
+Составить две программы, моделирующие процесс. Одна из программ
+должна использовать класс ArrayList, а вторая — LinkedList. Какая из двух
+программ работает быстрее? Почему?
+ */
+package B_1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class B_1_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> persons = new ArrayList<>();
+        for (int i = 0; i <= 10; i++) {
+            persons.add(i);
+        }
+        System.out.println(persons);
+
+        while (persons.size() > 1) {
+            Iterator<Integer> iterator = persons.iterator();
+            for (int i = 0; iterator.hasNext(); i++, iterator.next()) {
+                if (i % 2 == 1) {
+                    iterator.remove();
+                    System.out.println(persons);
+                }
+            }
+        }
+    }
+}
