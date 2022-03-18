29f9d5552295554978efc6b96565fb7b01d8d489
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-03-18 18:18:52 +0300

    B_2 - format code

--- src/B_2/B_2.java
+++ src/B_2/B_2.java
@@ -11,30 +11,27 @@ import java.util.ArrayList;
 public class B_2 {
     public static void main(String[] args) {
         ArrayList<Integer> integers = new ArrayList<>();
-        int x = 30;
+        integers.add(-14);
         integers.add(1);
         integers.add(22);
         integers.add(5);
         integers.add(30);
+        integers.add(-44);
         integers.add(3);
         integers.add(40);
         integers.add(4);
+        integers.add(-4);
+        integers.add(-54);
         System.out.println(integers);
 
-        int temp1 = integers.get(2);
-        integers.set(2, integers.get(1));
-        integers.set(1, temp1);
-        System.out.println(integers);
-
-        for (int i = 0; i < integers.size(); i++) {
-            for (int j = 1; j < (integers.size() - i); j++) {
-                if (integers.get(j - 1) > x) {
-                    int temp = integers.get(j - 1);
-                    integers.set(j - 1, integers.get(j));
-                    integers.set(j, temp);
+        for (int i = 1; i < integers.size(); i++)
+            for (int j = integers.size() - 1; j >= i; --j) {
+                if (integers.get(j - 1) > integers.get(j)) {
+                    integers.set(j - 1, integers.get(j - 1) + integers.get(j));
+                    integers.set(j, integers.get(j - 1) - integers.get(j));
+                    integers.set(j - 1, integers.get(j - 1) - integers.get(j));
                 }
             }
-        }
         System.out.println(integers);
     }
 }
