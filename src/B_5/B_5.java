0429d3b4bc0a797a88a07ac58bddf98b73166c5d
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-03-18 18:25:04 +0300

    B_5 - task done

--- src/B_5/B_5.java
+++ src/B_5/B_5.java
@@ -6,8 +6,36 @@
  */
 package B_5;
 
-public class B_5 {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class B_5 {
+
+    private static final ArrayList<Integer> myList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        myList.add(1);
+        myList.add(3);
+        myList.add(5);
+        myList.add(7);
+        myList.add(1);
+        myList.add(9);
+        myList.add(20);
+        myList.add(30);
+        System.out.println(find(14));
+
+    }
+
+    public static Integer find(Integer number) {
+        Iterator<Integer> iterator = myList.iterator();
+        Integer min = iterator.next();
+
+        while (iterator.hasNext()) {
+            Integer elem = iterator.next();
+            if (Math.abs(elem - number) < Math.abs(min - number)) {
+                min = elem;
+            }
+        }
+        return min;
     }
 }
