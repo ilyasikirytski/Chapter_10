f21e6bed68f93531311521ac6ddb5032d805876a
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-03-31 16:31:35 +0300

    A_2 - task done

--- src/A_2/A_2.java
+++ src/A_2/A_2.java
@@ -2,7 +2,10 @@ package A_2;/*
 2. Ввести число, занести его цифры в стек. Вывести число, у которого цифры
 идут в обратном порядке.
  */
-
+/*
+разбить число на цифры, занести в стек. вывести в обратном порядке
+peak(). pop()
+ */
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -11,13 +14,15 @@ public class A_2 {
         Stack<Integer> numbers = new Stack<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextInt()) {
-            numbers.push(scanner.nextInt());
-        }
-        for (Integer i : numbers) {
-            String reverse = new StringBuffer(i.toString()).reverse().toString();
-            if (i > Integer.parseInt(reverse)) {
-                System.out.println(i);
+            String number = scanner.next();
+            String digital;
+            for (int i = 0; i < number.length(); i++) {
+                digital = number.substring(i, i + 1);
+                numbers.push(Integer.parseInt(digital));
             }
+        }
+        while (numbers.size() > 0) {
+            System.out.print(numbers.pop());
         }
     }
 }
