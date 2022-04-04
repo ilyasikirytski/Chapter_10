ad5f27e2f9832d27417a5f3146ed629fae14e176
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-04-04 15:25:17 +0300

    A_2 task done on review

--- src/A_2/A_2.java
+++ src/A_2/A_2.java
@@ -6,20 +6,23 @@ package A_2;/*
 разбить число на цифры, занести в стек. вывести в обратном порядке
 peak(). pop()
  */
+
 import java.util.Scanner;
 import java.util.Stack;
 
 public class A_2 {
     public static void main(String[] args) {
-        Stack<Integer> numbers = new Stack<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextInt()) {
             String number = scanner.next();
-            String digital;
-            for (int i = 0; i < number.length(); i++) {
-                digital = number.substring(i, i + 1);
-                numbers.push(Integer.parseInt(digital));
-            }
+            reverseNumber(number);
+        }
+    }
+
+    public static void reverseNumber(String number) {
+        Stack<Character> numbers = new Stack<>();
+        for (int i = 0; i < number.length(); i++) {
+            numbers.push(number.charAt(i));
         }
         while (numbers.size() > 0) {
             System.out.print(numbers.pop());
